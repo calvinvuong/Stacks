8ab35ff7554d3e319901d864eef0b650e40fe31d
Calvin Vuong <cvuong@stuy.edu>
2016-04-01 15:10:51 -0400

    LLStack done

--- LLStack.java
+++ LLStack.java
@@ -10,8 +10,29 @@ import java.util.LinkedList;
 
 public class LLStack<T> implements Stack<T> {
 
+    LinkedList<T> _stack;
+    
+    public LLStack() {
+	_stack = new LinkedList<T>();
+    }
 
+    public boolean isEmpty() {
+	return _stack.size() <= 0;
+    }
 
+    public T peek() {
+	if ( isEmpty() ) return null;
+	return _stack.getLast();
+    }
+
+    public T pop() {
+	if ( isEmpty() ) return null;
+	return _stack.removeLast();
+    }
+
+    public void push(T x) {
+	_stack.addLast(x);
+    }
 
     //main method for testing
     public static void main( String[] args ) {
